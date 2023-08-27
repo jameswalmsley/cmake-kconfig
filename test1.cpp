118fdf1c239b09b222dd8aeeac8c431ccedc1ec1
Dmitrii Lebed <lebed.dmitry@gmail.com>
2023-08-27 16:03:42 -0700

    Extend the example by adding few binaries to test

---
+++ test1.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+#include <autoconf.h>
+
+int main()
+{
+    printf("Test1 %s\n", CONFIG_STR_ID);
+
+    return 0;
+}
---
+++ test2.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+#include <autoconf.h>
+
+int main()
+{
+    printf("Test2 %s\n", CONFIG_STR_ID);
+
+    return 0;
+}
