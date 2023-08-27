d0880675ab358498ccc2eabc374ce02a165a27d3
Dmitrii Lebed <lebed.dmitry@gmail.com>
2023-08-27 16:03:46 -0700

    Port improved import_kconfig version
    Mostly from 9228c379 Zephyr commit

--- cmake/extensions.cmake
+++ cmake/extensions.cmake
@@ -97,45 +97,86 @@ endfunction()
 
 # 2.2 Misc
 #
-# import_kconfig(<prefix> <kconfig_fragment> [<keys>])
+# import_kconfig(<prefix> <kconfig_fragment> [<keys>] [TARGET <target>])
 #
 # Parse a KConfig fragment (typically with extension .config) and
 # introduce all the symbols that are prefixed with 'prefix' into the
 # CMake namespace. List all created variable names in the 'keys'
 # output variable if present.
+#
+# <prefix>          : symbol prefix of settings in the Kconfig fragment.
+# <kconfig_fragment>: absolute path to the config fragment file.
+# <keys>            : output variable which will be populated with variable
+#                     names loaded from the kconfig fragment.
+# TARGET <target>   : set all symbols on <target> instead of adding them to the
+#                     CMake namespace.
 function(import_kconfig prefix kconfig_fragment)
-    # Parse the lines prefixed with 'prefix' in ${kconfig_fragment}
+    cmake_parse_arguments(IMPORT_KCONFIG "" "TARGET" "" ${ARGN})
     file(
-        STRINGS
-        ${kconfig_fragment}
-        DOT_CONFIG_LIST
-        REGEX "^${prefix}"
-        ENCODING "UTF-8"
+            STRINGS
+            ${kconfig_fragment}
+            DOT_CONFIG_LIST
+            ENCODING "UTF-8"
     )
 
-    foreach (CONFIG ${DOT_CONFIG_LIST})
-        # CONFIG could look like: CONFIG_NET_BUF=y
+    foreach (LINE ${DOT_CONFIG_LIST})
+        if("${LINE}" MATCHES "^(${prefix}[^=]+)=([ymn]|.+$)")
+            # Matched a normal value assignment, like: CONFIG_NET_BUF=y
+            # Note: if the value starts with 'y', 'm', or 'n', then we assume it's a
+            # bool or tristate (we don't know the type from <kconfig_fragment> alone)
+            # and we only match the first character. This is to align with Kconfiglib.
+            set(CONF_VARIABLE_NAME "${CMAKE_MATCH_1}")
+            set(CONF_VARIABLE_VALUE "${CMAKE_MATCH_2}")
+        elseif("${LINE}" MATCHES "^# (${prefix}[^ ]+) is not set")
+            # Matched something like: # CONFIG_FOO is not set
+            # This is interpreted as: CONFIG_FOO=n
+            set(CONF_VARIABLE_NAME "${CMAKE_MATCH_1}")
+            set(CONF_VARIABLE_VALUE "n")
+        else()
+            # Ignore this line.
+            # Note: we also ignore assignments which don't have the desired <prefix>.
+            continue()
+        endif()
 
-        # Match the first part, the variable name
-        string(REGEX MATCH "[^=]+" CONF_VARIABLE_NAME ${CONFIG})
+        # If the provided value is n, then the corresponding CMake variable or
+        # target property will be unset.
+        if("${CONF_VARIABLE_VALUE}" STREQUAL "n")
+            if(DEFINED IMPORT_KCONFIG_TARGET)
+                set_property(TARGET ${IMPORT_KCONFIG_TARGET} PROPERTY "${CONF_VARIABLE_NAME}")
+            else()
+                unset("${CONF_VARIABLE_NAME}" PARENT_SCOPE)
+            endif()
+            list(REMOVE_ITEM keys "${CONF_VARIABLE_NAME}")
+            continue()
+        endif()
 
-        # Match the second part, variable value
-        string(REGEX MATCH "=(.+$)" CONF_VARIABLE_VALUE ${CONFIG})
-        # The variable name match we just did included the '=' symbol. To just get the
-        # part on the RHS we use match group 1
-        set(CONF_VARIABLE_VALUE ${CMAKE_MATCH_1})
-
-        if("${CONF_VARIABLE_VALUE}" MATCHES "^\"(.*)\"$") # Is surrounded by quotes
+        # Otherwise, the variable/property will be set to the provided value.
+        # For string values, we also remove the surrounding quotation marks.
+        if("${CONF_VARIABLE_VALUE}" MATCHES "^\"(.*)\"$")
             set(CONF_VARIABLE_VALUE ${CMAKE_MATCH_1})
         endif()
 
-        set("${CONF_VARIABLE_NAME}" "${CONF_VARIABLE_VALUE}" PARENT_SCOPE)
+        if(DEFINED IMPORT_KCONFIG_TARGET)
+            set_property(TARGET ${IMPORT_KCONFIG_TARGET} PROPERTY "${CONF_VARIABLE_NAME}" "${CONF_VARIABLE_VALUE}")
+        else()
+            set("${CONF_VARIABLE_NAME}" "${CONF_VARIABLE_VALUE}" PARENT_SCOPE)
+        endif()
         list(APPEND keys "${CONF_VARIABLE_NAME}")
     endforeach()
 
-    foreach(outvar ${ARGN})
-        set(${outvar} "${keys}" PARENT_SCOPE)
-    endforeach()
+    if(DEFINED IMPORT_KCONFIG_TARGET)
+        set_property(TARGET ${IMPORT_KCONFIG_TARGET} PROPERTY "kconfigs" "${keys}")
+    endif()
+
+    list(LENGTH IMPORT_KCONFIG_UNPARSED_ARGUMENTS unparsed_length)
+    if(unparsed_length GREATER 0)
+        if(unparsed_length GREATER 1)
+            # Two mandatory arguments and one optional, anything after that is an error.
+            list(GET IMPORT_KCONFIG_UNPARSED_ARGUMENTS 1 first_invalid)
+            message(FATAL_ERROR "Unexpected argument after '<keys>': import_kconfig(... ${first_invalid})")
+        endif()
+        set(${IMPORT_KCONFIG_UNPARSED_ARGUMENTS} "${keys}" PARENT_SCOPE)
+    endif()
 endfunction()
 
 ########################################################
